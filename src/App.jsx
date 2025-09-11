3a497b76d34dca00c0d7d021e369bdfbdd2cffcd
hamza44962 <319556@wdz.ozhw.nl>
2025-09-11 13:36:31 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello TownMC 🚀
+      </h1>
+      <p className="text-gray-700 mt-4">
+        Dit is je React + Vite + Tailwind setup!
+      </p>
+    </div>
+  )
+}
+
+export default App;
